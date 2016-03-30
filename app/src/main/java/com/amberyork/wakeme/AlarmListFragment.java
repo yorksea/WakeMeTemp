2826dc31bb743c6c994f4ace75664ff813853c55
Amber York <yorksea@gmail.com>
2016-03-30 10:41:29 -0400

    Alarm List Fragment integrated, needs to have added elements within each item still.

---
+++ app/src/main/java/com/amberyork/wakeme/AlarmListFragment.java
@@ -0,0 +1,35 @@
+package com.amberyork.wakeme;
+
+import android.annotation.SuppressLint;
+import android.app.ListFragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class AlarmListFragment extends ListFragment implements OnItemClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.alarm_list, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Alarms, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/amberyork/wakeme/MenuFragment.java
+++ app/src/main/java/com/amberyork/wakeme/MenuFragment.java
@@ -7,13 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
 
 public class MenuFragment  extends Fragment {
 
